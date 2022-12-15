647e928a51772bad0520a7d757f552518bd7bd7f
Dustin Jones <dustintaylorjones@gmail.com>
2022-12-15 11:34:41 -0500

    a

--- percy/playwright-local.config.js
+++ percy/playwright-local.config.js
@@ -7,8 +7,8 @@ export const config = {
   testDir: 'tests',
   testMatch: '**/*.spec.js',
   // Use globalSetup & globalTearedown only if browserstack.local = true
-  globalSetup: require.resolve('./global-setup'),
-  globalTeardown: require.resolve('./global-teardown'),
+  // globalSetup: require.resolve('./global-setup'),
+  // globalTeardown: require.resolve('./global-teardown'),
   timeout: 60000,
   use:{
     viewport: null
