5ff4afd700e64f9015e3c0d587ebd48739b16c66
Dustin Jones <dustintaylorjones@gmail.com>
2023-06-08 18:56:24 -0400

    fix: remove script driver

--- src/setup.js
+++ src/setup.js
@@ -8,7 +8,7 @@ import { FPTI } from './constants';
 import { initLogger, checkForCommonErrors, setLogLevel, stringifyError,
     stringifyErrorMessage, getResourceLoadTime, isPayPalDomain, isEligible,
     getDomainSetting, once, openMetaFrame, precacheRememberedFunding,
-    getCurrentScript } from './lib';
+    getCurrentScript, checkForDeprecatedIntegration } from './lib';
 import { createPptmScript } from './external';
 import { Button } from './button';
 import { Checkout } from './checkout';
@@ -154,6 +154,8 @@ export let init = once(({ precacheRemembered }) => {
     if (!isPayPalDomain()) {
         createPptmScript();
     }
+
+    window.addEventListener('load', checkForDeprecatedIntegration);
 
     if (precacheRemembered) {
         precacheRememberedFunding();
